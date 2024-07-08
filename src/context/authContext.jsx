b98cf93353cc5da497481bbda81900d3b60b5d97
vinaya panasa <vinayapanasa@gmail.com>
2024-07-08 17:58:55 +0530

    Your commit message

---
+++ src/context/authContext.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) =>{
+
+    const navigate = useNavigate();
+
+
+    const login = async (inputs) =>{
+
+        try{
+            await axios.post('http://localhost:6001/auth/login', inputs)
+            .then( async (res)=>{
+                console.log("HIT LOGIN")
+                console.log(res.data);
+                if(res.status==200){
+                    await localStorage.setItem('userToken', res.data.token);
+                    await localStorage.setItem('userId', res.data.user._id);
+                    await localStorage.setItem('userName', res.data.user.username);
+                    await localStorage.setItem('userEmail', res.data.user.email);
+                }
+                console.log(res)
+                navigate('/');
+            }).catch((err) =>{
+                console.log(err);
+            });
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+
+    const register = async (inputs) =>{
+
+        try{
+            await axios.post('http://localhost:6001/auth/register', inputs)
+            .then( async (res)=>{
+                await localStorage.setItem('userToken', res.data.token);
+                await localStorage.setItem('userId', res.data.user._id);
+                await localStorage.setItem('userName', res.data.user.username);
+                await localStorage.setItem('userEmail', res.data.user.email);
+                navigate('/');
+            }).catch((err) =>{
+                console.log(err);
+            });
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+
+
+    const logout = async () =>{
+        await localStorage.setItem('userToken', null);
+        await localStorage.setItem('userId', null);
+        await localStorage.setItem('userName', null);
+        await localStorage.setItem('userEmail', null);
+        navigate('/');
+    }
+
+
+    
+
+    return(
+        <AuthContext.Provider value={{login, register, logout}}>{children}</AuthContext.Provider>
+    )
+
+
+}
